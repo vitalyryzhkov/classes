6872e39fdd40f8f1e3b3d353906e9b5b9f8513cb
vitalyryzhkov <pentagon0555@gmail.com>
2017-05-17 18:58:20 +0300

    Create professor.py

---
+++ professor.py
@@ -0,0 +1,10 @@
+class Professor:
+    def __init__(self, name):
+        self.name = name
+        self.courses = []
+
+    def add_course(self, course):
+        pass
+
+    def print_info(self):
+        pass
