a22e10df6a637ba720493c2734d5f7d0f1a244ad
vitalyryzhkov <pentagon0555@gmail.com>
2017-05-17 18:58:01 +0300

    Create course.py

---
+++ course.py
@@ -0,0 +1,11 @@
+class Course:
+
+    def __init__(self, name):
+        self.name = name
+        self.lecturers = []
+
+    def add_lecturer(self, lecturer):
+        pass
+
+    def print_info(self):
+        pass
