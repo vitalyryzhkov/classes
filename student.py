7ff40c6553e2b31cbc8a520692b9aebe0cdafd13
vitalyryzhkov <pentagon0555@gmail.com>
2017-05-21 19:55:16 +0300

    Update student.py

--- student.py
+++ student.py
@@ -1,69 +1,48 @@
-class Student:
-    name = ""
-    age = 18
-    year = 1
-    papers = []
-    car = ""
-    achievements = None  # if there is nothing reaasonable
+import person
 
-    def __init__(self, name, car, age=18, year=1):
-        print("new student")
-        self.name = name
+class Student(person.Person):
+    # name = ""
+    # age = 18
+    # year = 1
+    # papers = []
+    # car = ""
+    # achievements = None  # if there is nothing reaasonable
+
+    def __init__(self, name, car="BMW", age=18, year=1):
+        person.Person.__init__(self, name)
+        # print("new student")
+        # self.name = name
         self.age = age
         self.year = year
         self.car = car
 
     # @staticmethod
     def print_info(self, lang="EN"):
-        if lang == "EN":
-            print("Name: ", self.name)
-            print("Year: ", self.year)
-            print("Age: ", self.age)
-            print("Car:", self.car)
-        elif lang == "UA":
-            print("Имя: ", self.name)
-            print("Курс: ", self.year)
-            print("Возраст: ", self.age)
-            print("Авто:", self.car)
+        #
+        # i18n = {
+        #     "EN": ["Name: ", "Year: ", "Age: ", "Car:"],
+        #     "UA": ["Имя: ", "Год: ", "Возраст: ", "Авто:"]
+        # }
+        # return i18n
 
-    # def print_papers(self):
-    #     for idx, paper in enumerate(self.papers):
-    #         print("Paper %d: %s" % (idx, paper))
+        # if lang == "EN":
+        #     print("Name: ", self.name)
+        #     print("Year: ", self.year)
+        #     print("Age: ", self.age)
+        #     print("Car:", self.car)
+        # elif lang == "UA":
+        #     print("Имя: ", self.name)
+        #     print("Курс: ", self.year)
+        #     print("Возраст: ", self.age)
+        #     print("Авто:", self.car)
+        person.Person.print_info(self)
+        print("Age: %s" % self.age)
+        print("Year: %s" % self.year)
+        print("Car: %s" % self.car)
 
-    # def print_info_ua(self):
-    #     print("Имя: ", self.name)
-    #     print("Курс: ", self.year)
-    #     print("Возраст: ", self.age)
-
-# student = Student()
-# student2 = Student()
-
-student = Student("Alice", "MB", 19, 1)
-student2 = Student("Bob", "BMW", 22, 3)
-
-# print(type(student))
-
-# print(id(student))
-# print(id(student2))
-
-student.name = "Alice"
-student2.name = "Bob"
-
-# print(student.name)
-# print(student2.name)
-
-# Student.print_info(student)
-# Student.print_info(student2)
-
-# student.print_info()
-# student2.print_info()
-
-# student.papers.append("Math")
-# student2.papers.append("Literature")
-# student.print_papers()
-# student2.print_papers()
+    def print_papers(self):
+        for idx, paper in enumerate(self.papers):
+            print("Paper %d: %s" % (idx, paper))
 
 
-student.print_info()
-student2.print_info("UA")
 
