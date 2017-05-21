ffbf723ae1a5350d7692f582574e8198417ac73f
vitalyryzhkov <pentagon0555@gmail.com>
2017-05-21 19:54:57 +0300

    Update professor.py

--- professor.py
+++ professor.py
@@ -1,10 +1,18 @@
-class Professor:
+import person
+
+class Professor(person.Person):
     def __init__(self, name):
-        self.name = name
+        person.Person.__init__(self, name)
+        # self.name = name
         self.courses = []
 
     def add_course(self, course):
-        pass
+        if course not in self.courses:
+            self.courses.append(course)
 
     def print_info(self):
-        pass
+        # print("Lecture: %s" % self.name)
+        person.Person.print_info(self)
+        print("Courses: ")
+        for course in self.courses:
+            print("\t%s" % course.name)
