e4e5171b6b35d2be9927b059c26145509a7d30a3
vitalyryzhkov <pentagon0555@gmail.com>
2017-05-24 18:49:26 +0300

    Update student.py

--- student.py
+++ student.py
@@ -1,48 +1,33 @@
 import person
 
+#------------------------------------------------
+# class type, id
 class Student(person.Person):
-    # name = ""
-    # age = 18
-    # year = 1
-    # papers = []
-    # car = ""
-    # achievements = None  # if there is nothing reaasonable
 
-    def __init__(self, name, car="BMW", age=18, year=1):
+    def __init__(self, name, age=18, year=1):
         person.Person.__init__(self, name)
-        # print("new student")
+        print("I'm new Student")
         # self.name = name
-        self.age = age
+        self.age  = age
         self.year = year
-        self.car = car
+        self.papers_local = []
 
     # @staticmethod
     def print_info(self, lang="EN"):
-        #
         # i18n = {
-        #     "EN": ["Name: ", "Year: ", "Age: ", "Car:"],
-        #     "UA": ["Имя: ", "Год: ", "Возраст: ", "Авто:"]
+        #     "EN": ["Name", "Year", "Age"],
+        #     "UA": ["Им'я", "Рiк",  "Вiк"],
+        #     "IT": ["Name", "Year", "Age"],
+        #     "FR": ["Name", "Year", "Age"],
         # }
-        # return i18n
-
-        # if lang == "EN":
-        #     print("Name: ", self.name)
-        #     print("Year: ", self.year)
-        #     print("Age: ", self.age)
-        #     print("Car:", self.car)
-        # elif lang == "UA":
-        #     print("Имя: ", self.name)
-        #     print("Курс: ", self.year)
-        #     print("Возраст: ", self.age)
-        #     print("Авто:", self.car)
+        #
+        # print(i18n[lang][0], ": ", self.name)
+        # print(i18n[lang][1], ": ", self.year)
+        # print(i18n[lang][2], ": ", self.age)
         person.Person.print_info(self)
-        print("Age: %s" % self.age)
-        print("Year: %s" % self.year)
-        print("Car: %s" % self.car)
+        print(self._format_row ("Year", self.year))
+        print(self._format_row ("Age", self.year))
 
     def print_papers(self):
-        for idx, paper in enumerate(self.papers):
-            print("Paper %d: %s" % (idx, paper))
-
-
-
+        for idx, paper in enumerate(self.papers_local):
+            print("Paper %d: %s" %(idx, paper))
