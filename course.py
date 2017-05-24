d5c104ff0069b9bd0be7c4bc459dc5491b3bc23b
vitalyryzhkov <pentagon0555@gmail.com>
2017-05-24 18:48:04 +0300

    Update course.py

--- course.py
+++ course.py
@@ -8,10 +8,8 @@ class Course:
         if lecturer not in self.lecturers:
             self.lecturers.append(lecturer)
 
-
     def print_info(self):
-        print("Course title: %s" % self.name )
-        print("Lecturers: ")
+        print("Course title: %s" % self.name)
+        print("Lecturers:")
         for lecturer in self.lecturers:
             print("\t%s" % lecturer.name)
-
