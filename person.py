8bac0c501aebc07e8b98f51225d95aa09da844bc
vitalyryzhkov <pentagon0555@gmail.com>
2017-05-24 18:50:05 +0300

    Update person.py

--- person.py
+++ person.py
@@ -1,22 +1,27 @@
 class Person:
 
-    FORMAT_ATTR = "%-10s"
-    FORMAT_VALUE = "%20s"
-    FORMAT_ALL = FORMAT_ATTR + ": " + FORMAT_VALUE
+    __FORMAT_ATTR = "%-10s"
+    __FORMAT_DELIM = ":"
+    __FORMAT_VALUE = "%20s"
+    __FORMAT_ALL = __FORMAT_ATTR + __FORMAT_VALUE
 
-    total_count = 0
+    __total_count = 0
 
     def __init__(self, name, email="", phone_number=""):
         self.name = name
         self.email = email
         self.phone_number = phone_number
-        Person.total_count += 1
-        self.id = Person.total_count
+        Person.__total_count += 1
+        self.__id = Person.__total_count
 
-    def print_info(self):
+    def _format_row(self, attr_name, attr_value):
+        attr_name_delim = attr_name + Person.__FORMAT_DELIM
+        return Person.__FORMAT_ALL % (attr_name_delim, attr_value)
+
+    def print_info(self, lang="EN"):
         print()
-        print("----------------------------")
-        print((Person.FORMAT_ALL) % ("ID", self.id))
-        print((Person.FORMAT_ALL) % ("Name", self.name))
-        print(("Email: " + Person.FORMAT_ATTR) % self.email)
-        print(("Phone number: " + Person.FORMAT_ATTR) % self.phone_number)
+        print("-----------------------------")
+        print(self._format_row ("ID", self.__id))
+        print(self._format_row ("Name", self.name))
+        print(self._format_row ("Email", self.email))
+        print(self._format_row ("Phone", self.phone_number))
